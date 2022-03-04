1b1edd830b1d8d16aad93db598480ff93ff7f066
BrayanR09 <72059121+BrayanR09@users.noreply.github.com>
2022-03-04 16:48:46 -0600

    Versión 1
    Primera versión

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using ConsoleApp1;
+
+Console.WriteLine("Primera parte");
+Console.WriteLine("-------------");
+
+
+//var date1 = new Date(2022, 3, 4);
+
+Date date1 = new Date(2022, 3, 4);
+Console.WriteLine(date1);
